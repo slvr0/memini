e0277293fc473ac324220617a2d03e876182f2e9
Dan Johansson <johansson_dan@hotmail.com>
2024-06-02 21:03:21 +0200

    Fixed the react component to even communicate with API end point

--- memini-react/memini-react/src/App.js
+++ memini-react/memini-react/src/App.js
@@ -1,25 +1,57 @@
+import React, { Component, useEffect } from "react";
+
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component{
+    constructor(props){
+        super(props);  
+        
+        this.API_URL = "http://localhost:5000/";
+        this.state = {
+          note: []
+        };      
+    }
+    
+  fetchData = async () => {
+    fetch(this.API_URL + "api/Calendar/GetCalendar")
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Failed to fetch data');
+      }
+      return response.json(); 
+    })
+    .then(data => {
+      this.setState({note:data});
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
+  }
+  componentDidMount() {
+ 
+    this.fetchData();
+  }
+
+  render() { 
+      
+        const note = this.state.note;
+
+        return (
+          <>
+          {JSON.stringify(note)}
+          </>
+
+        );
+    }
+  }
 
 export default App;
+
+
+
+
+
+
+
+
