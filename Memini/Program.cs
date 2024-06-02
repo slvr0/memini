fbfef257b4996bd2fafc4830772917b79476f0bf
Dan Johansson <johansson_dan@hotmail.com>
2024-06-02 20:33:03 +0200

    Init

---
+++ Memini/Program.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Builder;
+using Newtonsoft.Json.Serialization;
+using Memini.Controllers;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "My API", Version = "v1" });
+});
+
+//JSON Serializer
+builder.Services.AddControllersWithViews().AddNewtonsoftJson(options =>
+options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+).AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver = new DefaultContractResolver());
+
+builder.Services.AddControllers();
+
+var app = builder.Build();
+
+app.UseCors(c => c.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+app.UseStaticFiles();
+
+app.UseSwagger();
+
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+});
+
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/")
+    {
+        context.Response.Redirect("/swagger");
+        return;
+    }
+    await next();
+});
+
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.MapControllerRoute(
+    name: "Memini",
+    pattern: "{controller=Calendar}/{action=GetCalendar}");
+
+app.Run();
---
+++ Memini/controllers/CalendarController.cs
@@ -0,0 +1,30 @@
+﻿
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+
+namespace Memini.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CalendarController : ControllerBase
+    {
+
+        private IConfiguration _configuration;
+        public CalendarController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+
+        [HttpGet]
+        [Route("GetCalendar")]
+        public JsonResult GetCalendar()
+        {
+            return new JsonResult("Durp");
+        }
+
+    }
+}
+
