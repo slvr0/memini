364e35414d1b054e17d3a3cc41cf1de572f9a9b2
Dan Johansson <johansson_dan@hotmail.com>
2024-11-24 22:00:42 +0100

    Made a little login page

--- memini-react/memini-react/src/components/user/components/user-main-page.jsx
+++ memini-react/memini-react/src/components/user/components/user-main-page.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Input } from 'semantic-ui-react'
 
 class UserMainPage extends Component{
     constructor(props){
@@ -9,8 +10,122 @@ class UserMainPage extends Component{
     render() { 
 
         return (
-            <>  
-                <h3> Welcome to your user page! mr....</h3>
+            <> 
+           
+           
+                <div className="ui row titleRow">
+                    <div className="four wide column">
+                       
+                    </div>
+
+                    <div className="four wide column">
+                        
+                    </div>
+
+                    <div className="four wide column">
+                    <div>
+
+                    </div>
+                    </div>
+
+                    <div className="four wide column">
+                        <div>
+
+                        </div>
+                    </div>                  
+
+                </div>
+
+                <div className="ui row">
+                    <div className="four wide column">
+                        
+                    </div>
+                    <div className="eight wide column">
+                        <div className="ui icon input login-name-input">
+                            <input
+                                type="text"                                                   
+                                placeholder="Your name..."
+                            />                        
+                        </div>
+                    </div>
+                    <div className="four wide column">
+                        
+                    </div>
+                </div>
+
+                <div className="ui row">
+                    <div className="four wide column">
+                        
+                    </div>
+                    <div className="eight wide column">
+                        <div className="ui icon input login-name-input">
+                            <input
+                                type="text"                                                   
+                                placeholder="Your Email..."
+                            />                        
+                        </div>
+                    </div>
+                    <div className="four wide column">
+                        
+                    </div>
+                </div>
+
+                <div className="ui row">
+                    <div className="four wide column">
+                        
+                    </div>
+                    <div className="four wide column">                      
+                        
+                        <div className="ui button big basic user-main-login-button">
+                            Login                       
+                        </div>
+
+                        <div className="ui button big basic user-main-login-button">
+                            I am god                       
+                        </div>
+                      
+                    </div>
+                    <div className="four wide column">
+                        <div className="ui button small default user-main-create-account-button">
+                                Create Account                       
+                        </div>
+                    </div>
+
+
+                    <div className="four wide column">
+                        
+                    </div>
+                </div>
+
+                <div className="ui row">
+                    <div className="four wide column">                        
+                        
+                    </div>
+
+                    <div className="eight wide column">
+                        <div className="ui divider"></div>
+                    </div>
+
+                    <div className="four wide column">
+                        
+                    </div>                    
+                </div>
+
+                <div className="ui row">
+                    <div className="four wide column">                        
+                        
+                    </div>
+
+                    <div className="eight wide column">
+                        <i className="user-main-forgot-password">Forgot password?</i>
+                    </div>
+
+                    <div className="four wide column">
+                        
+                    </div>                    
+                </div>
+
+          
             </>       
         );
     }
